02e293ce3b213a8fb7f89a173eff226fdd7ea2bc
Khammunh Thao <100525451+Khammunh@users.noreply.github.com>
2024-09-25 16:30:05 +0700

    update code Get

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_interpolation_to_compose_strings
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:crud/firebase_database/database_methods.dart';
 import 'package:crud/screen/add_new_data.dart';
@@ -11,6 +13,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+   TextEditingController nameController = TextEditingController();
+  TextEditingController professionController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
   Stream? userStream;
   getInitiateUser() async {
     userStream = await DatabaseMethods().getUserData();
@@ -33,45 +38,88 @@ class _HomeScreenState extends State<HomeScreen> {
                 itemBuilder: (context, index) {
                   DocumentSnapshot userData = snapShot.data.docs[index];
                   return SizedBox(
-        height: 130.0,
-        width: MediaQuery.of(context).size.width,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Card(
-            color: Colors.purple.shade50,
-            shape: RoundedRectangleBorder(
-              side: BorderSide(
-                color: Colors.purple.shade700,
-                width: 2.0,
-              ),
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            child:   Padding(
-              padding: EdgeInsets.all(12.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Name :'+userData['Name'],
-                    style:
-                        TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    'Profession :'+userData['Profession'],
-                    style:
-                        TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    'Age:'+userData['Age'],
-                    style:
-                        TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      );
+                    height: 180.0,
+                    width: MediaQuery.of(context).size.width,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        color: Colors.purple.shade50,
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                            color: Colors.purple.shade700,
+                            width: 2.0,
+                          ),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Name :' + userData['Name'],
+                                style: const TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                'Profession :' + userData['Profession'],
+                                style: const TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                'Age:' + userData['Age'],
+                                style: const TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        editUserDetail(
+                                          userData['Id'],
+                                        );
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.purple.shade100,
+                                      ),
+                                      child: const Text(
+                                        'Edit',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16.0,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(width: 10.0),
+                                  Expanded(
+                                    child: ElevatedButton(
+                                      onPressed: () {},
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.purple.shade500,
+                                        foregroundColor: Colors.white,
+                                      ),
+                                      child: const Text(
+                                        'Delete',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16.0,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
                 })
             : Container();
       },
@@ -108,5 +156,131 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: allUserDataList(),
     );
+  }
+
+  Future editUserDetail(String id) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Column(
+              children: [
+                Row(
+                  children: [
+                    const Text(
+                      "Edit User Detail",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Icon(
+                        Icons.close,
+                      ),
+                    )
+                  ],
+                ),
+                const Text(
+              'Name',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 5.0),
+            Container(
+              padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+              decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10.0)),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            const SizedBox(height: 5.0),
+            const Text(
+              'Profession',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 5.0),
+            Container(
+              padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+              decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10.0)),
+              child: TextField(
+                controller: professionController,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            const SizedBox(height: 5.0),
+            const Text(
+              'Age',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 5.0),
+            Container(
+              padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+              decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10.0)),
+              child: TextField(
+                controller: ageController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            Center(
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: InkWell(
+                  onTap: () async {
+                    // String id = randomAlphaNumeric(8);
+                    // Map<String, dynamic> userData = {
+                    //   "Name": nameController.text,
+                    //   "Profession": professionController.text,
+                    //   "Id": id,
+                    //   "Age": ageController.text,
+                    // };
+                    // await DatabaseMethods()
+                    //     .addUserData(userData, id)
+                    //     .then((value) {
+                    //   ShowToasts()
+                    //       .getToast("Data Saved Successfully", Colors.green);
+                    // });
+                    // Navigator.pop(context);
+                  },
+                  child: const Text(
+                    "Update User",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          
+              ],
+            ),
+          );
+        });
   }
 }
